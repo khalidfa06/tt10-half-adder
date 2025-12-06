a03193e5916ebbf5c713b37b5e3f77f4dd41515a
khalidfa06 <fatima.khalid2306@gmail.com>
2025-12-05 21:15:55 -0500

    Update and rename project.v to tt_um_khalid_fatima_half_adder.v

--- src/tt_um_khalid_fatima_half_adder.v
+++ src/tt_um_khalid_fatima_half_adder.v
@@ -1,11 +1,6 @@
-/*
- * Copyright (c) 2024 Your Name
- * SPDX-License-Identifier: Apache-2.0
- */
-
 `default_nettype none
 
-module tt_um_example (
+module tt_um_khalid_fatima_half_adder (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -16,12 +11,28 @@ module tt_um_example (
     input  wire       rst_n     // reset_n - low to reset
 );
 
+    wire x = ui_in[0];
+    wire y = ui_in[1];
+
+    wire S, C;
+
+    assign S = x ^ y; // xor for sum
+    assign C = x & y; // and for carry
+    
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+    assign uo_out[0] = S;
+    assign uo_out[1] = C;
+    assign uo_out[2] = 1'b0;
+    assign uo_out[3] = 1'b0;
+    assign uo_out[4] = 1'b0;
+    assign uo_out[5] = 1'b0;
+    assign uo_out[6] = 1'b0;
+    assign uo_out[7] = 1'b0;
+    
+    assign uio_out = 0;
+    assign uio_oe  = 0;
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+    wire _unused = &{ena, clk, rst_n, ui_in[7:2], uio_in, 1'b0};
 
 endmodule

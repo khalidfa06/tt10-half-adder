a440b231ac129fd81a1d182ca9badff2921f3198
khalidfa06 <fatima.khalid2306@gmail.com>
2025-12-05 22:04:14 -0500

    Update test.py

--- test/test.py
+++ test/test.py
@@ -32,29 +32,42 @@ async def test_project(dut):
     # Wait for one clock cycle to see the output values
     await ClockCycles(dut.clk, 1)
 
-    # The following assersion is just an example of how to check the output values.
-    # Change it to match the actual expected output of your module:
+    # Assert the actual expected output of your test case:
     assert dut.uo_out[0].value == 0
     assert dut.uo_out[1].value == 0
     # Keep testing the module by changing the input values, waiting for
     # one or more clock cycles, and asserting the expected output values.
-
     # Repeat for the remaining 3 test cases
 
+    # Set the input values you want to test
     dut.ui_in[0].value = 0
     dut.ui_in[1].value = 1
+
+    # Wait for one clock cycle to see the output values
     await ClockCycles(dut.clk, 1)
-    assert dut.uo_out[0].value == 1
-    assert dut.uo_out[1].value == 0
-    
-    dut.ui_in[0].value = 1
-    dut.ui_in[1].value = 0
-    await ClockCycles(dut.clk, 1)
+
+    # Assert the actual expected output of your test case:
     assert dut.uo_out[0].value == 1
     assert dut.uo_out[1].value == 0
 
+    # Set the input values you want to test
+    dut.ui_in[0].value = 1
+    dut.ui_in[1].value = 0
+    
+    # Wait for one clock cycle to see the output values
+    await ClockCycles(dut.clk, 1)
+
+    # Assert the actual expected output of your test case:
+    assert dut.uo_out[0].value == 1
+    assert dut.uo_out[1].value == 0
+
+    # Set the input values you want to test
     dut.ui_in[0].value = 1
     dut.ui_in[1].value = 1
+
+    # Wait for one clock cycle to see the output values
     await ClockCycles(dut.clk, 1)
+
+    # Assert the actual expected output of your test case:
     assert dut.uo_out[0].value == 0
     assert dut.uo_out[1].value == 1
